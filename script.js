2d9997d5c61b74bcf48b43647233455f5387eb47
Puneet Singh <99singhpunee99@gmail.com>
2020-12-23 19:24:38 +0530

    Add leapyear checking, new design
    Added leapyear checking functionality and a complete redesign

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 
 const btnLeapYear = document.querySelector("#btn-checkLeapYear");
 
+function clickHandler(){
+    const inputDob = document.querySelector("#input-dob");
+    const date = new Date(inputDob.value);
+    const year = date.getFullYear();
 
-btnLeapYear.addEventListener("click", () => {
-    const dob = document.querySelector("#input-dob");
-    const output = document.querySelector("#h2-output");
-    output.innerHTML = dob.value;
-});
+    
+    let yearOutput;    
+    if(year % 400 === 0){
+        yearOutput = "Leap Year";
+    } else if(year % 100 === 0){
+        yearOutput = "Not a Leap Year";
+    } else if(year % 4 === 0){
+        yearOutput = "Leap Year";
+    } else{
+        yearOutput = "Not a Leap Year";
+    }
+
+    const outputLeapYear = document.querySelector("#p-outputYear");
+    outputLeapYear.innerHTML = yearOutput;
+}
+
+
+btnLeapYear.addEventListener("click", clickHandler);
